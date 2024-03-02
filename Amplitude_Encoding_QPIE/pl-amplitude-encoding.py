7329e7e849b18cf3e907fc8fb26630f725bb212a
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2024-03-02 21:48:21 +0200

    Fix relative imports for penylane files

--- Amplitude_Encoding_QPIE/pl-amplitude-encoding.py
+++ Amplitude_Encoding_QPIE/pl-amplitude-encoding.py
@@ -5,28 +5,21 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-# To import my custom funcitons
-import FeaturePlayground.Direct_sum_util as Direct_sum_util
-from typing import Any, Optional, Union
+
 from pennylane.measurements import StateMP
+from typing import Any, Optional, Union
 
-def pad_with_zeros(arr: np.ndarray, number_of_zeros: Optional[int] = None) -> np.ndarray:
-    """
-    Pad an np.array with a specified number of zeros at the end.
+# Add the parent directory of the current script's directory to the Python path
+import sys
+import os
 
-    Parameters:
-        arr (numpy.ndarray): Input array.
-        number_of_zeros (int, optional): Number of zeros to add at the end of the array.
-            If not provided, the number of zeros will be calculated based on the next power of 2.
-    
-    Returns:
-        numpy.ndarray: Padded array.
-    """
-    if number_of_zeros is None:
-        number_of_zeros = int(2 ** np.ceil(np.log2(len(arr))) - len(arr))
-    
-    return np.pad(arr, (0, number_of_zeros), mode='constant')
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))  # Get the directory of the current script
+sys.path.append(os.path.dirname(SCRIPT_DIR))  # Add the parent directory to the Python path
 
+# To import my custom functions
+# Import Local modules
+from Utilities.utils import pad_with_zeros
+import FeaturePlayground.Direct_sum_util as Direct_sum_util
 
 
 
