91b5a64787eacc63971cb21c347b8f5946820ad1
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2024-03-07 23:03:03 +0200

    Extra unit test

--- Utilities/utils.py
+++ Utilities/utils.py
@@ -1,5 +1,5 @@
-from pathlib import Path
-print('Running' if __name__ == '__main__' else 'Importing', Path(__file__).resolve())
+# from pathlib import Path
+# print('Running' if __name__ == '__main__' else 'Importing', Path(__file__).resolve())
 
 import numpy as np
 
