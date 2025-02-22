841c3582bae88bb0f21431d166e1f4f934aea019
Nick <80918856+Nick-Liou@users.noreply.github.com>
2025-02-22 03:28:58 +0200

    Update encoding_test.py

--- Unit_tests/encoding_test.py
+++ Unit_tests/encoding_test.py
@@ -159,10 +159,7 @@ def test_Encodings_multiple_cases(encoding_function: Callable , expected_stateve
         # print(f"case: {case}")
         _, result = encode_data(case, encoding_function)
         state_vector = result.get_statevector().data
-        expected_statevector = expected_statevector_gen(case)        
-        print(f"\nCase: {case}")
-        print(f"state_vector: {state_vector}")
-        print(f"expected_statevector: {expected_statevector}")
+        expected_statevector = expected_statevector_gen(case)
         assert np.allclose(state_vector, expected_statevector, atol=TOLERANCE)
 
     # Test with multiple randomly generated inputs
